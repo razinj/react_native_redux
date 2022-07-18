674cdf0bfc07f8b9181f70709dd94837a40c7ead
razinj <nizarjailane@gmail.com>
2022-07-18 21:22:56 +0100

    Add persistence

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import {StatusBar, useColorScheme, View} from 'react-native';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import Counter from './Counter';
-import {store} from './store';
+import {persistor, store} from './store';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <Provider store={store}>
-      <View>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <Counter />
-      </View>
+      <PersistGate loading={null} persistor={persistor}>
+        <View>
+          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+          <Counter />
+        </View>
+      </PersistGate>
     </Provider>
   );
 };
